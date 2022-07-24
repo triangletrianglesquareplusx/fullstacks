b8b0ead0d8f12f83a268610a7fa10869928aa418
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-07-24 11:01:32 +0300

    basic react skeleton

---
+++ part 1/part1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => (
+  <div>
+    <p>Hello world</p>
+    <Naming />
+  </div>
+)
+
+const Naming = () => (
+  <h1>This is a name</h1>
+)
+
+export default App
+
