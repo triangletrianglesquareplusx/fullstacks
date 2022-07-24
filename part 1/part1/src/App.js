eb3edaf190b76a71f5bfc0d4ebb715ea929dfa0b
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-07-24 12:14:05 +0300

    solved first react exercises

--- part 1/part1/src/App.js
+++ part 1/part1/src/App.js
@@ -1,15 +1,39 @@
-import './App.css';
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-    <Naming />
-  </div>
-)
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  
 
-const Naming = () => (
-  <h1>This is a name</h1>
-)
+  return (
+    <div>
+      <Header info={course}/>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </div>
+    
+  )
+}
+
+const Header = (props) =><h1>{props.info}</h1>;
+
+const Content = (props)=> {
+  return (
+    <div>
+      <Part part={props.part1} exercise={props.exercises1}/>
+      <Part part={props.part2} exercise={props.exercises2}/>
+      <Part part={props.part3} exercise={props.exercises3}/>
+    </div>
+  )
+}
+
+const Total = (props)=><p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+
+const Part = (props)=><p>{props.part} {props.exercise}</p>
 
 export default App
 
